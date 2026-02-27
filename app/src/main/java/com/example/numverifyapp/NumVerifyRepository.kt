5406da6854191fb0c20a0e9a9e0721b93546edca
Silethemba Dube <sledube@gmail.com>
2026-02-27 13:17:48 +0200

    added exception handling

--- app/src/main/java/com/example/numverifyapp/NumVerifyRepository.kt
+++ app/src/main/java/com/example/numverifyapp/NumVerifyRepository.kt
@@ -1,25 +1,60 @@
 package com.example.numverifyapp
-
 import com.example.numverifyapp.Models.Country
 import com.example.numverifyapp.Models.Telephone
 import com.example.numverifyapp.RetrofitClient.retrofitClient
 import retrofit2.Call
 import retrofit2.Response
+import java.io.IOException
 
-const val accessKey = "b089608159a93022fff45fbd58960dee"
+const val accessKey = "b089608159a93022fff45fbd58960de"
+
 class NumVerifyRepository {
 
     fun getCountryCodeList(): Map<String, Country>? {
-        val countryCodeList: Call<Map<String, Country>?> =
-            retrofitClient.getCountryList(accessKey = accessKey)
-        val countryCodeResponse: Response<Map<String, Country>?> = countryCodeList.execute()
-        return countryCodeResponse.body()
+        return try {
+            val countryCodeList: Call<Map<String, Country>?> =
+                retrofitClient.getCountryList(accessKey = accessKey)
+            val countryCodeResponse: Response<Map<String, Country>?> = countryCodeList.execute()
+
+            if (countryCodeResponse.isSuccessful) {
+                countryCodeResponse.body()
+            } else {
+                //Handle API error
+                println("Error: ${countryCodeResponse.errorBody()?.string()}")
+                null
+            }
+        } catch (e: IOException) {
+            //Handle Network errors
+            println("Error: ${e.message}")
+            null
+        } catch (e: Exception) {
+            //Handle any other exceptions
+            println("Error: ${e.message}")
+            null
+        }
     }
 
     fun validateNumber(number: String, countryCode: String): Telephone? {
-        val validateNumber: Call<Telephone?> =
-            retrofitClient.validateNumber(number, countryCode, "1", accessKey = accessKey)
-        val validateNumberResponse: Response<Telephone?> = validateNumber.execute()
-        return validateNumberResponse.body()
+        return try {
+            val validateNumber: Call<Telephone?> =
+                retrofitClient.validateNumber(number, countryCode, "1", accessKey = accessKey)
+            val validateNumberResponse: Response<Telephone?> = validateNumber.execute()
+
+            if (validateNumberResponse.isSuccessful) {
+                validateNumberResponse.body()
+            } else {
+                //Handle API error
+                println("Error: ${validateNumberResponse.errorBody()?.string()}")
+                null
+            }
+        } catch (e: IOException) {
+            //Handle Network errors
+            println("Error: ${e.message}")
+            null
+        } catch (e: Exception) {
+            //Handle any other exceptions
+            println("Error: ${e.message}")
+            null
+        }
     }
 }
